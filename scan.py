32bc1edb6395e0912dfdd41c67a6cb52af915edc
Alex Corre <alex@workpop.com>
2018-06-18 19:28:52 -0700

    feat(logs): log combined result + DRY out url a bit

--- scan.py
+++ scan.py
@@ -137,19 +137,22 @@ def lambda_handler(event, context):
     verify_s3_object_version(s3_object)
     sns_start_scan(s3_object)
     file_path = download_s3_object(s3_object, "/tmp")
+    s3_display_url = os.path.join(s3_object.bucket_name, s3_object.key)
 
     # ClamAV
     clamav.update_defs_from_s3(AV_DEFINITION_S3_BUCKET, AV_DEFINITION_S3_PREFIX)
     clamscan_result = clamav.scan_file(file_path)
-    print("[ClamAV] Scan of s3://%s resulted in %s\n" % (os.path.join(s3_object.bucket_name, s3_object.key), clamscan_result))
+    print("[ClamAV] Scan of s3://%s resulted in %s\n" % (s3_display_url, clamscan_result))
 
     # olevba
     ole_result = ole.scan_file(file_path)
-    print("[olevba] Scan of s3://%s resulted in %s\n" % (os.path.join(s3_object.bucket_name, s3_object.key), ole_result))
+    print("[olevba] Scan of s3://%s resulted in %s\n" % (s3_display_url, ole_result))
 
     # If any supported scans are infected, consider the file infected
     results = [clamscan_result, ole_result]
     scan_result = AV_STATUS_INFECTED if AV_STATUS_INFECTED in results else AV_STATUS_CLEAN
+
+    print("Combined result of s3://%s result is %s" % (s3_display_url, scan_result))
 
     # store results
     if "AV_UPDATE_METADATA" in os.environ:
